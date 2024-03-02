da6337f1a4a001a9f5e6b18aedab191d8fddf165
Scorredorg23 <160347094+Scorredorg23@users.noreply.github.com>
2024-03-02 05:46:18 +0000

    Siu

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Holaclase espero que esto este saliendo bien</h1>
+    <p>Hola mundo</p>
+    <img src='https://upload.wikimedia.org/wikipedia/en/4/48/Mor%2C_No_Le_Temas_A_La_Oscuridad.jpeg' ></img>
+    </>
+  );
+}
+
+export default App;
